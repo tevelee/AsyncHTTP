177be64ae857a1e28a87af530a7fd0dafe84d92b
Laszlo Teveli <tevelee@gmail.com>
2022-05-02 12:14:16 +0200

    Add supported platforms

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,10 @@ let package = Package(
     name: "AsyncHTTP",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12)
+        .macOS(.v12),
+        .macCatalyst(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(name: "AsyncHTTP", targets: ["AsyncHTTP"]),
